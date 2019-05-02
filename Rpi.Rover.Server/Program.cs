2a432b39c0cf58d23e556b366f7a60bb0285c50c
Dave Glover <dglover@microsoft.com>
2019-05-02 11:01:51 +1000

    added mqtt as a listener to he rover server

--- Rpi.Rover.Server/Program.cs
+++ Rpi.Rover.Server/Program.cs
@@ -2,6 +2,11 @@
 using System.Device.Gpio;
 using System.Diagnostics;
 using System.IO;
+using uPLibrary.Networking.M2Mqtt;
+using uPLibrary.Networking.M2Mqtt.Messages;
+using System.Net;
+using System.Text;
+
 
 namespace Rpi.listen
 {
@@ -36,9 +41,27 @@ namespace Rpi.listen
         static Motor left = new Motor(controller, (int)MotorMap.TwoPlus, (int)MotorMap.TwoMinus);
         static Motor right = new Motor(controller, (int)MotorMap.OnePlus, (int)MotorMap.OneMinus);
 
+        static MqttClient client = new MqttClient("localhost");
+
+
         static void Main(string[] args)
         {
+            client.MqttMsgPublishReceived += client_MqttMsgPublishReceived;
+
+
+            string clientId = Guid.NewGuid().ToString();
+            client.Connect(clientId);
+
+            // subscribe to the topic "/home/temperature" with QoS 2 
+            client.Subscribe(new string[] { "/rover/motor" }, new byte[] { MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE });
+
             tcp.Listen(roverActions);
+        }
+
+        static void client_MqttMsgPublishReceived(object sender, MqttMsgPublishEventArgs e)
+        {
+            string action = Encoding.UTF8.GetString(e.Message, 0, e.Message.Length);
+            roverActions(action);
         }
 
         static void roverActions(string action)
